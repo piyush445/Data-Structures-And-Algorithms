a6916f6159be51e5b043963a1e44f860f2c476bc
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2020-10-01 15:47:18 +0530

    Update Reverse of Lists.py
    Here i am pointing some error in your code
    1. You are passing "li" in method reverse_l( li ) which is not declared yet . You have to pass "n" as an argument (Line no.11)
    2. In Line number 12 you are trying to print "li" instead of "n"
    3. In Line number 7 and 9 you are trying to assign value into string type object which is wrong.

--- Arrays/Reverse of Lists.py
+++ Arrays/Reverse of Lists.py
@@ -1,12 +1,21 @@
-## If You Don't Know What A list is then: List is colllection of data that can be of any Type.
-## first We Create A fucntion
-def reverse_l(li):
-    length= len(li)
-    first=list(n.split())
-    for a in range(len(first)): ## This Range Is Converting the String input into int 
-        li[a]=int(first[a])
-    for i in range(length//2):
-        li[i],li[(length-i)-1]=li[(length-i)-1],li[i]
-n=input("Enter Numbers given by space: ") ## here i Specify to Give Space otherwise 12345 will be considerd as one string not numbers and then cannot be splitted 
-reverse_l(li)                             ## Program Can Be Modified With More Flexibilty 
-print(li)
+# If You Don't Know What A list is then: List is collection of data that can be of any Type.
+# first We Create A fucntion
+# Method 1 
+def reverse(arr):
+    length = len(arr)  # size of the list 
+    for i in range(length//2):  # It iterates til mid of the list and swaps elements
+        arr[i] , arr[(length-i)-1] = arr[(length-i)-1] , arr[i]
+n = input("Enter Numbers given by space: ")  # here i Specify to Give Space otherwise 12345 will be considerd as one string not numbers and then cannot be splitted 
+n = n.split() # It splits the string. Defalut parameter is space (" ")
+arr = [int(e) for e in n] # It converts string element to int and append into arr
+reverse(arr) 
+print(arr)
+
+#Time Complexity = O(n) + O(n/2) = O(n)
+#Space Complexity = O(n)
+
+# Method 2
+
+n = list(map(int,input("Enter number by space").split())) # It takes space seperated integer values and map to list
+n = n[::-1]    # It slices the list in reverse order
+print(n)
